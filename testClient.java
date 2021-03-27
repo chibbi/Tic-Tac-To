39c8c033f18f516bbd59b44bd9a3eb1708a432d5
chibbi <chehab.djibril@gmail.com>
2021-03-27 01:15:32 +0100

    Cleaned up (removed .class files)

--- testClient.java
+++ testClient.java
@@ -6,44 +6,76 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.util.logging.Formatter;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
 
 class testClient {
 
     private static DatagramSocket socket;
     private static InetAddress address;
-    private static boolean isActive = true;
+    private static Logger logger;
 
     public static void main(String[] args) {
-        System.out.println("Client started");
+        logger = Logger.getGlobal();
+        setFormatter();
+        logger.info("Client started");
         try {
             socket = new DatagramSocket();
             address = InetAddress.getByName("localhost");
         } catch (UnknownHostException eu) {
-            System.out.println("Couldn't find localhosts address");
-            System.out.println(eu.getMessage());
+            logger.log(Level.SEVERE, "Couldn't find localhosts address");
+            logger.log(Level.SEVERE, eu.getMessage());
         } catch (SocketException es) {
-            System.out.println("Couldn't connect to Server");
-            System.out.println(es.getMessage());
+            logger.log(Level.SEVERE, "Couldn't connect to Server");
+            logger.log(Level.SEVERE, es.getMessage());
         }
-        InputThread input = new InputThread(socket, address);
-        byte[] buf = new byte[512];
-        DatagramPacket packet = new DatagramPacket(buf, buf.length);
-        while (isActive) {
-            new Thread(input).start();
-            // clean buffer
-            buf = new byte[512];
-            packet = new DatagramPacket(buf, buf.length);
-            try {
-                socket.receive(packet);
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
+        InputThread input = new InputThread(socket, address, logger);
+        OutputThread output = new OutputThread(socket, logger);
+        new Thread(input).start();
+        new Thread(output).start();
+        // TODO: if wanting to close, clsoe (if possible (easily, do it inside of
+        // input))
+        // and then do this =>
+        // logger.info("Server stopped");
+        // socket.close();
+    }
+
+    // TODO: format that output of logging to read it easier
+    private static void setFormatter() {
+        Formatter formatter = new Formatter() {
+
+            @Override
+            public String format(LogRecord logRecord) {
+                return String.format("Level: %s, message: %s", logRecord.getLevel(), logRecord.getMessage());
             }
-            String received = new String(packet.getData(), 0, packet.getLength());
-            System.out.println(received);
-        }
-        System.out.println("Server stopped");
-        socket.close();
+
+        };
+        Handler handler = new Handler() {
+
+            @Override
+            public void close() throws SecurityException {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void flush() {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void publish(LogRecord arg0) {
+                // TODO Auto-generated method stub
+
+            }
+
+        };
+        handler.setFormatter(formatter);
+        logger.addHandler(handler);
     }
 }
 
@@ -52,37 +84,70 @@ class InputThread extends Thread {
     private InetAddress address;
     private byte[] buf;
     private DatagramPacket packet;
+    private Logger logger;
 
-    InputThread(DatagramSocket socketIn, InetAddress addressIn) {
+    InputThread(DatagramSocket socketIn, InetAddress addressIn, Logger loggerIn) {
         this.socket = socketIn;
         this.address = addressIn;
+        this.logger = loggerIn;
     }
 
     public void run() {
-        // clean buffer
-        buf = new byte[512];
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String input = "";
-        try {
-            input = reader.readLine();
-            if (input == "") {
+        while (true) {
+            // clean buffer
+            buf = new byte[512];
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            String input = "";
+            try {
                 input = reader.readLine();
+                if (input == "") {
+                    input = reader.readLine();
+                }
+            } catch (IOException e) {
+                logger.log(Level.SEVERE, "Couldn't read your Input");
+                logger.log(Level.SEVERE, e.getLocalizedMessage());
             }
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+            if (input.equals("/stopclient")) {
+                // TODO: Stop the whole Client (i may should just put the output in a thread and
+                // leave input in main)
+            }
+            buf = input.getBytes();
+            packet = new DatagramPacket(buf, buf.length, address, 5678);
+            try {
+                socket.send(packet);
+            } catch (IOException e) {
+                logger.log(Level.SEVERE, "Couldn't send your Message");
+                logger.log(Level.SEVERE, e.getLocalizedMessage());
+            }
         }
-        if (input.equals("/stopclient")) {
-            // TODO: Stop the whole Client (i may should just put the output in a thread and
-            // leave input in main)
-        }
-        buf = input.getBytes();
-        packet = new DatagramPacket(buf, buf.length, address, 5678);
-        try {
-            socket.send(packet);
-        } catch (IOException e1) {
-            // TODO Auto-generated catch block
-            e1.printStackTrace();
+    }
+}
+
+class OutputThread extends Thread {
+    private DatagramSocket socket;
+    private byte[] buf;
+    private DatagramPacket packet;
+    private Logger logger;
+
+    OutputThread(DatagramSocket socketIn, Logger loggerIn) {
+        this.socket = socketIn;
+        this.logger = loggerIn;
+    }
+
+    public void run() {
+        while (true) {
+            // clean buffer
+            buf = new byte[512];
+            packet = new DatagramPacket(buf, buf.length);
+            // try reading
+            try {
+                socket.receive(packet);
+            } catch (IOException e) {
+                logger.log(Level.SEVERE, "Couldn't read Servers Message");
+                logger.log(Level.SEVERE, e.getLocalizedMessage());
+            }
+            String received = new String(packet.getData(), 0, packet.getLength());
+            logger.info(received);
         }
     }
 }
